ca52a1c256b1ab2108d634d2e79055ae11951755
jespieee <mhuhu@umich.edu>
2022-11-15 19:10:48 -0500

    added update project function, but has bug

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,7 +1,11 @@
 import { Link, useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import ClientInfo from '../components/ClientInfo';
 import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQueries';
+import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
+
 
 export default function Project() {
   const { id } = useParams();
@@ -20,6 +24,16 @@ export default function Project() {
           w-25 d-inline ms-auto'>
             Back
           </Link>
+
+          <h1>{data.project.name}</h1>
+          <p>{data.project.description}</p>
+
+          <h5 className='mt-3'>Project Status</h5>
+          <p className='lead'>{data.project.status}</p>
+
+          <ClientInfo client={data.project.client} />
+          <EditProjectForm project={data.project} />
+          <DeleteProjectButton projectId={id} />
         </div>
       ) }
     </>);
